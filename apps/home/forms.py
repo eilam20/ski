7b3e876da9bd9a28eaf5e6f4914aeb41e9d1535c
eilam <eilam7200@gmail.com>
2025-02-12 12:04:13 +0200

    update date

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm, DateInput, NumberInput
+from django import forms
 
 from apps.home.models import Order
 
 
 class OrderCreateModelForm(ModelForm):
+    return_date = forms.DateField(
+        required=True,
+        label="תאריך החזרה",
+        widget=DateInput(
+            format='%d/%m/%Y',
+            attrs={'class': 'form-control', 'placeholder': 'בחר תאריך', 'type': 'date', }
+        ),
+        error_messages={'required': 'יש לבחור תאריך החזרה!'}
+    )
     class Meta:
         model = Order
         fields = ['name',
@@ -20,6 +30,15 @@ class OrderCreateModelForm(ModelForm):
 
 
 class OrderUpdateModelForm(ModelForm):
+    return_date = forms.DateField(
+        required=True,
+        label="תאריך החזרה",
+        widget=DateInput(
+            format='%d/%m/%Y',
+            attrs={'class': 'form-control', 'placeholder': 'בחר תאריך', 'type': 'date'}
+        ),
+        error_messages={'required': 'יש לבחור תאריך החזרה!'}
+    )
     class Meta:
         model = Order
         fields = ['name',
